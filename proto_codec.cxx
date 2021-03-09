b64660b8da0cd2592cc606921e428c4dba7bec60
NoevilMe <surpass168@live.com>
2021-03-09 18:03:51 +0800

    add protobuf codec

---
+++ proto_codec.cxx
@@ -0,0 +1,256 @@
+#include "proto_codec.h"
+
+#include <zlib.h>
+
+namespace muduo {
+namespace event_loop {
+
+#ifdef __WIN32
+inline u_short networkToHost16(u_short netshort) { return ntohs(netshort); }
+
+inline u_long networkToHost32(u_long netlong) { return ntohl(netlong); }
+
+inline u_short hostToNetwork16(u_short netshort) { return htons(netshort); }
+
+inline u_long hostToNetwork32(u_long netlong) { return htonl(netlong); }
+
+inline unsigned __int64 networkToHost64(unsigned __int64 value) {
+    return ntohll(value);
+}
+
+inline unsigned __int64 hostToNetwork64(unsigned __int64 value) {
+    return htonll(value);
+}
+#else
+// refer to https://linux.die.net/man/3/be16toh
+inline uint16_t networkToHost16(uint16_t net16) { return be16toh(net16); }
+
+inline uint32_t networkToHost32(uint32_t net32) { return be32toh(net32); }
+
+inline uint16_t hostToNetwork16(uint16_t host16) { return htobe16(host16); }
+
+inline uint32_t hostToNetwork32(uint32_t host32) { return htobe32(host32); }
+
+inline uint64_t hostToNetwork64(uint64_t host64) { return htobe64(host64); }
+
+inline uint64_t networkToHost64(uint64_t net64) { return be64toh(net64); }
+#endif
+
+ProtobufCodec::ProtobufCodec(const ProtobufMessageCallback &cb)
+    : message_cb_(cb), error_cb_(DefaultErrorCallback) {}
+
+ProtobufCodec::ProtobufCodec(const ProtobufMessageCallback &cb,
+                             const ErrorCallback &ecb)
+    : message_cb_(cb), error_cb_(ecb) {}
+
+ProtobufCodec::~ProtobufCodec() {}
+
+void ProtobufCodec::OnMessageData(const std::string &data,
+                                  Timestamp timestamp) {
+    left_data_.append(data);
+
+    while (left_data_.length() >= kMinMessageBodyLen + kHeaderLen) {
+        ProtoMsgHeaderType len = NetworkAsUint32(left_data_.data());
+
+        if (len > kMaxMessageBodyLen || len < kMinMessageBodyLen) {
+            error_cb_(left_data_, timestamp, kInvalidLength);
+            break;
+        } else if (left_data_.length() >=
+                   static_cast<size_t>(len + kHeaderLen)) {
+            ErrorCode errorCode = kNoError;
+            MessagePtr message =
+                ParseBody(left_data_.data() + kHeaderLen, len, &errorCode);
+
+            if (errorCode == kNoError && message) {
+                message_cb_(message, timestamp);
+                left_data_ = left_data_.substr(len + kHeaderLen);
+            } else {
+                error_cb_(left_data_, timestamp, errorCode);
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+}
+
+void ProtobufCodec::OnMessage(const char *data, int len, Timestamp timestamp) {
+
+    const char *pos = data;
+    const char *end = data + len;
+    while (pos < data + len) {
+        uint32_t body_len = NetworkAsUint32(pos);
+        pos += sizeof(body_len);
+
+        if (body_len > kMaxMessageBodyLen || len < kMinMessageBodyLen) {
+            error_cb_(std::string(pos, end - pos), timestamp, kInvalidLength);
+            break;
+        } else if ((end - pos) >= body_len) {
+            ErrorCode errorCode = kNoError;
+            MessagePtr message = ParseBody(pos, body_len, &errorCode);
+
+            if (errorCode == kNoError && message) {
+                message_cb_(message, timestamp);
+                pos += body_len;
+            } else {
+                error_cb_(std::string(pos, end - pos), timestamp, errorCode);
+                break;
+            }
+        } else {
+            error_cb_(std::string(pos, end - pos), timestamp, kInvalidLength);
+            break;
+        }
+    }
+}
+
+std::string ProtobufCodec::ErrorCodeToString(ErrorCode code) {
+    switch (code) {
+    case ProtobufCodec::kNoError:
+        return "NoError";
+    case ProtobufCodec::kInvalidLength:
+        return "InvalidLength";
+    case ProtobufCodec::kInvalidNameLen:
+        return "InvalidNameLen";
+    case ProtobufCodec::kUnknownMessageType:
+        return "UnknownMessageType";
+    case ProtobufCodec::kParseError:
+        return "ParseError";
+    case ProtobufCodec::kCheckSumError:
+        return "CheckSumError";
+    default:
+        return "UnknownError";
+    }
+}
+
+void ProtobufCodec::PackMessage(const google::protobuf::Message &msg,
+                                std::string &bytes) {
+    auto type_name = msg.GetTypeName();
+    ProtoMsgNameLengthType type_name_len =
+        (ProtoMsgNameLengthType)type_name.length();
+    auto proto_bytes = msg.SerializeAsString();
+
+    //// total len
+    // ProtoMsgLengthType len = sizeof(ProtoMsgNameLengthType) + typeNameLen +
+    // (ProtoMsgLengthType)proto_bytes.length();
+    ////// msg type len
+    ////bytes.append((const char *)&type_name_len, sizeof(type_name_len));
+    ////// msg type
+    ////bytes.append(type_name);
+    ////// protobuf bytes
+    ////bytes.append(proto_bytes);
+
+    std::string body;
+    // msg type length
+    body.append((const char *)&type_name_len, sizeof(type_name_len));
+    // msg type
+    body.append(type_name);
+    // protobuf bytes
+    body.append(proto_bytes);
+
+    uint32_t check_sum = ::adler32(
+        1, reinterpret_cast<const Bytef *>(body.data()), body.length());
+    AppendUint32(check_sum, body);
+
+    ProtoMsgHeaderType body_len = body.length();
+    AppendUint32(body_len, bytes);
+    bytes.append(body);
+}
+
+MessagePtr ProtobufCodec::ParseBody(const char *buf, int len,
+                                    ErrorCode *error) {
+    MessagePtr message;
+
+    const char *pos = buf;
+    uint32_t expectedCheckSum = NetworkAsUint32(pos + len - kCheckSumLen);
+
+    uint32_t checkSum =
+        ::adler32(1, reinterpret_cast<const Bytef *>(pos), len - kCheckSumLen);
+    if (checkSum == expectedCheckSum) {
+
+        ProtoMsgNameLengthType nameLen = NetworkAsUint8(buf);
+        pos += sizeof(ProtoMsgNameLengthType);
+
+        if (nameLen >= kMinMsgNameLen &&
+            nameLen <= len - sizeof(ProtoMsgNameLengthType) - kCheckSumLen) {
+            std::string typeName(pos, nameLen);
+            pos += nameLen;
+
+            message.reset(CreateMessage(typeName));
+            if (message) {
+                auto size = len - sizeof(ProtoMsgNameLengthType) - nameLen -
+                            kCheckSumLen;
+                if (message->ParseFromArray(pos, (int)size)) {
+                    *error = kNoError;
+                } else {
+                    *error = kParseError;
+                }
+            } else {
+                *error = kUnknownMessageType;
+            }
+        } else {
+            *error = kInvalidNameLen;
+        }
+    } else {
+        *error = kCheckSumError;
+    }
+
+    return message;
+}
+
+uint32_t ProtobufCodec::NetworkAsUint32(const char *buf) {
+    uint32_t be32 = 0;
+#ifdef _WIN32
+    ::memcpy_s(&be32, sizeof(be32), buf, sizeof(be32));
+#else
+    ::memcpy(&be32, buf, sizeof(be32));
+#endif
+    return networkToHost32(be32);
+}
+
+uint8_t ProtobufCodec::NetworkAsUint8(const char *buf) {
+    uint8_t value = 0;
+    memcpy(&value, buf, sizeof(value));
+    return value;
+}
+
+void ProtobufCodec::AppendUint32(uint32_t value, std::string &data) {
+    uint32_t be32 =hostToNetwork32(value);
+    data.append((const char *)(&be32), sizeof(be32));
+}
+
+void ProtobufCodec::AppendUshort(unsigned short value, std::string &data) {
+    unsigned short be16 = hostToNetwork16(value);
+    data.append((const char *)(&be16), sizeof(be16));
+}
+
+google::protobuf::Message *
+ProtobufCodec::CreateMessage(const std::string &typeName) {
+    google::protobuf::Message *message = nullptr;
+
+    const google::protobuf::Descriptor *descriptor =
+        google::protobuf::DescriptorPool::generated_pool()
+            ->FindMessageTypeByName(typeName);
+    if (descriptor) {
+        auto prototype =
+            google::protobuf::MessageFactory::generated_factory()->GetPrototype(
+                descriptor);
+        if (prototype) {
+            message = prototype->New();
+        }
+    }
+
+    return message;
+}
+
+void ProtobufCodec::DefaultErrorCallback(const std::string &, Timestamp,
+                                         ErrorCode) {
+    throw std::string("Not implemented");
+}
+
+ProtobufDispatcher::ProtobufDispatcher(const ProtobufMessageCallback &defaultCb)
+    : default_callback_(defaultCb) {}
+
+ProtobufDispatcher::~ProtobufDispatcher() {}
+
+} // namespace event_loop
+} // namespace muduo
---
+++ proto_codec.h
@@ -0,0 +1,157 @@
+#ifndef __PROTO_CODEC_H_
+#define __PROTO_CODEC_H_
+
+#include "timestamp.h"
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <string>
+
+#include <google/protobuf/message.h>
+
+//|----header----|---------------------------body-----------------|--checksum--|
+//|----header----|--name len--|----name----|-------msg bytes------|--checksum--|
+//|---int32_t----|--uint8_t-_-|----char----|---------char---------|--checksum--|
+//
+// struct ProtobufTransportFormat __attribute__ ((__packed__))
+// {
+//   int32_t  len;
+//   int8_t   nameLen;
+//   char     typeName[nameLen];
+//   char     protobufData[len-nameLen-8];
+//   int32_t  checkSum; // adler32 of nameLen, typeName and protobufData
+// }
+
+namespace muduo {
+namespace event_loop {
+
+using MessagePtr = std::shared_ptr<google::protobuf::Message>;
+using ProtoMsgLengthType = int32_t;
+using ProtoMsgNameLengthType = uint8_t;
+using ProtoMsgHeaderType = ProtoMsgLengthType;
+
+constexpr auto kMinMsgNameLen = 1;
+
+class ProtobufCodec {
+public:
+    enum ErrorCode {
+        kNoError = 0,
+        kCheckSumError,
+        kInvalidLength,
+        kInvalidNameLen,
+        kUnknownMessageType,
+        kParseError
+    };
+
+    typedef std::function<void(const MessagePtr &, Timestamp)>
+        ProtobufMessageCallback;
+    typedef std::function<void(const std::string &, Timestamp, ErrorCode)>
+        ErrorCallback;
+
+    explicit ProtobufCodec(const ProtobufMessageCallback &cb);
+    ProtobufCodec(const ProtobufMessageCallback &cb, const ErrorCallback &ecb);
+    ~ProtobufCodec();
+
+    void OnMessageData(const std::string &data, Timestamp timestamp);
+    void OnMessage(const char *data, int len, Timestamp timestamp);
+
+    static std::string ErrorCodeToString(ErrorCode code);
+
+    void PackMessage(const google::protobuf::Message &msg, std::string &bytes);
+
+private:
+    // parse msg outer body
+    MessagePtr ParseBody(const char *buf, int len, ErrorCode *error);
+
+    uint32_t NetworkAsUint32(const char *buf);
+    uint8_t NetworkAsUint8(const char *buf);
+
+    void AppendUint32(uint32_t value, std::string &data);
+    void AppendUshort(unsigned short value, std::string &data);
+
+    google::protobuf::Message *CreateMessage(const std::string &typeName);
+    static void DefaultErrorCallback(const std::string &data,
+                                     Timestamp timestamp, ErrorCode code);
+
+    std::string left_data_;
+    ProtobufMessageCallback message_cb_;
+    ErrorCallback error_cb_;
+
+    const static int kHeaderLen =
+        sizeof(ProtoMsgHeaderType); // 4 bytes, message length
+    const static int kMinMessageBodyLen =
+        sizeof(ProtoMsgNameLengthType) +
+        kMinMsgNameLen; // name len + typename(>=1)
+    const static int kMaxMessageBodyLen =
+        64 * 1024 * 1024; // limited in protobuf source code
+    const static int kCheckSumLen = sizeof(int32_t);
+};
+
+/*
+ *
+ *  Dispatcher
+ *
+ */
+
+class Callback {
+public:
+    virtual ~Callback(){};
+    virtual void OnMessage(const MessagePtr &message, Timestamp timestamp) = 0;
+};
+
+template <typename T> class CallbackT : public Callback {
+public:
+    using ProtobufMessageTCallback = std::function<void(
+        const std::shared_ptr<T> &message, Timestamp timestamp)>;
+
+    CallbackT(const ProtobufMessageTCallback &callback) : callback_(callback) {}
+
+    virtual void OnMessage(const MessagePtr &message,
+                           long long timestamp) override {
+        std::shared_ptr<T> concrete = std::static_pointer_cast<T>(message);
+        callback_(concrete, timestamp);
+    }
+
+private:
+    ProtobufMessageTCallback callback_;
+};
+
+class ProtobufDispatcher {
+public:
+    using ProtobufMessageCallback =
+        std::function<void(const MessagePtr &msg, Timestamp timestamp)>;
+
+    explicit ProtobufDispatcher(const ProtobufMessageCallback &defaultCb);
+    ~ProtobufDispatcher();
+
+    void OnProtobufMesssage(const MessagePtr &message, Timestamp timestamp) {
+        auto it = callbacks_.find(message->GetDescriptor());
+
+        if (it != callbacks_.end()) {
+            it->second->OnMessage(message, timestamp);
+        } else {
+            default_callback_(message, timestamp);
+        }
+    }
+
+    template <typename T>
+    void registerMessageCallback(
+        const typename CallbackT<T>::ProtobufMessageTCallback &callback) {
+        std::shared_ptr<CallbackT<T>> pd(new CallbackT<T>(callback));
+        callbacks_[T::descriptor()] = pd;
+    }
+
+private:
+    typedef std::map<const google::protobuf::Descriptor *,
+                     std::shared_ptr<Callback>>
+        CallbackMap;
+
+    CallbackMap callbacks_;
+    ProtobufMessageCallback default_callback_;
+};
+
+} // namespace event_loop
+} // namespace muduo
+
+#endif
