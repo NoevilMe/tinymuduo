a01d01be8ecae67646868e3f183c91aa9468241d
Jason.Dunn <surpass168@live.com>
2021-12-22 20:45:42 +0800

    add global header

---
+++ eventloop.h
@@ -0,0 +1,12 @@
+#ifndef __MUDUO_EVENTLOOP_ALL_H_
+#define __MUDUO_EVENTLOOP_ALL_H_
+
+#include "event_loop.h"
+#include "poller.h"
+#include "proto_codec.h"
+#include "timer.h"
+#include "timespan.h"
+#include "timespec.h"
+#include "timestamp.h"
+
+#endif /* __MUDUO_EVENTLOOP_ALL_H_ */
